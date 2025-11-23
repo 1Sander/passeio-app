ccfd899441a13586c4921134560e254833a93b44
Wallace Sander <90098336+1Sander@users.noreply.github.com>
2025-11-22 21:56:05 -0300

    upd: formularios lugares

--- src/app/lugares/lugar/lugar.ts
+++ src/app/lugares/lugar/lugar.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CategoriaClass } from '../../categorias/categoria.class';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-lugar',
   standalone: false,
   templateUrl: './lugar.html',
-  styleUrl: './lugar.scss',
+  styleUrls: ['./lugar.scss'],
 })
-export class Lugar {
+export class Lugar implements OnInit {
 
+  camposForm: FormGroup;
+  categorias: CategoriaClass[] = [];
+
+  constructor( private categoriaService: CategoriaService) {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => this.categorias = listaCategorias
+    });
+  }
+
+
+  salvar() {
+    console.log('valores: ', this.camposForm.value);
+  }
 }
